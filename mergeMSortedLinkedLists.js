311668ec6d2e575ef3cde67357940aee106ab515
Colin Pace <colinpace0001@gmail.com>
2020-06-18 16:09:38 -0500

    merge m sorted linked lists

--- mergeMSortedLinkedLists.js
+++ mergeMSortedLinkedLists.js
@@ -31,6 +31,7 @@ class LinkedList {
       slow = firstList.head;
       let currentList = array.shift()
       let current = currentList.head;
+      let last = false;
 
       while (current) {
         if (current.data < fast.data) {
@@ -45,24 +46,38 @@ class LinkedList {
           }
 
         } else if (current.data === fast.data) {
+          if (last === true) break;
           const node = new Node(current.data);
           node.next = fast.next;
           fast.next = node;
           current = current.next;
-
         } else {
           while (fast.data < current.data) {
             if (fast.next) fast = fast.next;
-            else fast.next = new Node(current.data);
+            else {
+              fast.next = new Node(current.data);
+              last = true;
+            }
           }
 
           while (slow.next !== fast) slow = slow.next;
+
         }
       }
     }
 
     list = firstList;
     return list;
+  }
+
+  collectNodalData(list) {
+    const result = [];
+    let runner = list.head;
+    while (runner) {
+      result.push(runner.data);
+      runner = runner.next;
+    }
+    return result;
   }
 }
 
@@ -75,7 +90,7 @@ const integersB = [1, 3, 4];
 const integersC = [2, 6];
 
 // Another set of lists for input
-// const integersA = [2, 10, 12];
+// const integersA = [2, 3, 10, 12];
 // const integersB = [0, 3, 4];
 // const integersC = [1, 6];
 
@@ -87,4 +102,4 @@ const listsToMerge = [listA, listB, listC];
 const resultList = new LinkedList;
 
 const result = resultList.merge(resultList, listsToMerge);
-console.log(result.head.data);
+console.log(resultList.collectNodalData(result));
