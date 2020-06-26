eb322df95ced9365b1d26a29e1f84d55af144490
Colin Pace <colinpace0001@gmail.com>
2020-06-26 12:31:09 -0500

    bst traversal update

--- bstTraversals.js
+++ bstTraversals.js
@@ -1,4 +1,42 @@
-class Node {
+class QueueNode {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(data) {
+    let node = new QueueNode(data);
+    if (!this.head) {
+      this.tail = node;
+      this.head = this.tail;
+    } else {
+      this.head.next = node;
+      this.head = node;
+    }
+  }
+
+  dequeue() {
+    if (!this.head) return null;
+    else {
+      let node = this.tail;
+      if (this.tail === this.head) {
+        this.tail = null;
+        this.head = null;
+      } else this.tail = this.tail.next;
+      node.next = null;
+      return node;
+    }
+  }
+}
+
+class TreeNode {
   constructor(data, left = null, right = null) {
     this.data = data;
     this.left = left;
@@ -13,14 +51,14 @@ class BinarySearchTree {
 
   add(data) {
     let node = this.root;
-    if (!node) this.root = new Node(data);
+    if (!node) this.root = new TreeNode(data);
     else {
       const searchTree = function(node) {
         if (data < node.data) {
-          if (!node.left) node.left = new Node(data);
+          if (!node.left) node.left = new TreeNode(data);
           else return searchTree(node.left);
         } else if (data > node.data) {
-          if (!node.right) node.right = new Node(data);
+          if (!node.right) node.right = new TreeNode(data);
           else return searchTree(node.right);
         } else return null;
       }
@@ -31,7 +69,7 @@ class BinarySearchTree {
   inOrder() {
     if (!this.root) return null;
     else {
-      let result = [];
+      const result = [];
       const traversal = function(node) {
         node.left && traversal(node.left);
         result.push(` ${node.data}`);
@@ -45,7 +83,7 @@ class BinarySearchTree {
   preOrder() {
     if (!this.root) return null;
     else {
-      let result = [];
+      const result = [];
       const traversal = function(node) {
         result.push(` ${node.data}`);
         node.left && traversal(node.left);
@@ -59,7 +97,7 @@ class BinarySearchTree {
   postOrder() {
     if (!this.root) return null;
     else {
-      let result = [];
+      const result = [];
       const traversal = function(node) {
         node.left && traversal(node.left);
         node.right && traversal(node.right);
@@ -71,25 +109,30 @@ class BinarySearchTree {
   }
 
   levelOrder() {
-    let result = [];
-    let q = [];
-    if (this.root) {
-      q.push(this.root);
-      while(q.length) {
-        const node = q.shift();
-        result.push(` ${node.data}`);
-        if (node.left) q.push(node.left);
-        if (node.right) q.push(node.right);
+    if (!this.root) return null;
+    else {
+      const result = [];
+      queue.enqueue(this.root);
+      while (queue.head) {
+        const node = queue.dequeue().data;
+        result.push(" " + node.data);
+        if (node.left) queue.enqueue(node.left);
+        if (node.right) queue.enqueue(node.right);
       }
       return result;
-    } else return null;
+    }
   }
 }
 
-const numbers = new BinarySearchTree;
-let integers = [10, 5, 2, 8, 15, 12, 20];
-integers.forEach(integer => numbers.add(integer));
-console.log(`In order: ${numbers.inOrder()}`);
-console.log(`Pre order: ${numbers.preOrder()}`);
-console.log(`Post order: ${numbers.postOrder()}`);
-console.log(`Level order: ${numbers.levelOrder()}`);
+
+
+const queue = new Queue;
+const tree = new BinarySearchTree;
+
+const integers = [10, 5, 2, 8, 15, 12, 20];
+integers.forEach(integer => tree.add(integer));
+
+console.log(`In order: ${tree.inOrder()}`);
+console.log(`Pre order: ${tree.preOrder()}`);
+console.log(`Post order: ${tree.postOrder()}`);
+console.log(`Level order: ${tree.levelOrder()}`);
