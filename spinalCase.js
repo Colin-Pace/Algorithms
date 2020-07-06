61b333c9ef784e7fc84eacc7f8dbb6a11e6157af
Colin Pace <colinpace0001@gmail.com>
2020-07-06 12:14:36 -0500

    updates

--- spinalCase.js
+++ spinalCase.js
@@ -4,28 +4,24 @@ function spinalCase(str) {
   const l = str.length;
   let start, end, result = "";
 
-  const caseCheck = function(letter) {
+  for (let i = 0; i < l; i++) {
+    const integer = str.charCodeAt(i), letter = str[i];
     const upper = letter.toUpperCase(), lower = letter.toLowerCase();
     let result_, l_ = result.length - 1;
 
-    if (letter === upper && result[l_] === '-') result_ = lower;
-    else if (letter === upper) result_ = '-' + lower;
-    else result_ = lower;
-
-    return result_;
-  }
-
-  for (let i = 0; i < l; i++) {
-    const integer = str.charCodeAt(i), letter = str[i];
-
     if (i !== 0) {
-      if (90 >= integer && integer >= 65) result += caseCheck(letter);
-      else if (122 >= integer && integer >= 97) result += caseCheck(letter);
+      if (90 >= integer && integer >= 65) {
+        if (letter === upper && result[l_] === '-') result += lower;
+        else if (letter === upper) result += '-' + lower;
+        else result += lower;
+      }
+      else if (122 >= integer && integer >= 97) result += letter;
       else result += '-';
 
     } else {
       if (90 >= integer && integer >= 65) result += letter.toLowerCase();
       else if (122 >= integer && integer >= 97) result += letter.toLowerCase();
+      else result += '-';
     }
   }
 
