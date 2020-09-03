8c328bc627cfe37af0d0c015b3c5d7f50764e4ae
Colin Pace <colinpace0001@gmail.com>
2020-09-03 17:31:50 -0500

    iterative fibonacci added

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -1,16 +1,30 @@
-function fibonacciSequence(n, memo) {
+// Recursive
+function fibonacci(int, memo) {
   memo = memo || {};
-
-  if (memo[n]) {
-    return memo[n];
-  } else if (n < 1) {
-    return 0;
-  } else if (n <= 2) {
-    return 1;
-  } else {
-    memo[n] = fibonacciSequence(n - 1, memo) + fibonacciSequence(n - 2, memo);
-    return memo[n];
+  if (memo[int]) return memo[int];
+  else if (int < 1) return 0;
+  else if (int <= 2) return 1;
+  else {
+    memo[int] = fibonacci(int - 1, memo) + fibonacci(int - 2, memo);
+    return memo[int];
   }
 }
+console.log(fibonacci(10));
 
-console.log(fibonacciSequence(100));
+// Iterative
+function fibonacci(int) {
+  if (int === 0) return 0;
+  else if (int === 1) return 1;
+  else if (int === 2) return 1;
+  else {
+    let first = 1;
+    let second = 1;
+    for (let i = 3; i <= int; i++) {
+      let next = first + second;
+      if (i === int) return next
+      first = second;
+      second = next;
+    }
+  }
+}
+console.log(fibonacci(10));
