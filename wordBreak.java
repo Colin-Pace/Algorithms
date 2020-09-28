11e883aa444bcc1ca724092ca06f8bfda6ae1e36
Colin Pace <colinpace0001@gmail.com>
2020-09-28 06:48:42 -0500

    changed class name

--- wordBreak.java
+++ wordBreak.java
@@ -1,6 +1,6 @@
 /* LeetCode word break: Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words. The same word in the dictionary may be reused multiple times in the segmentation. You may assume the dictionary does not contain duplicate words. */
 
-public class WordBreak {
+public class wordBreak {
   static Boolean comparison(String[] dictionary, String letters) {
     int l = letters.length();
     int i = 0;                                                     // s iterator
