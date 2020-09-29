04338c3a0a5bf8e446a150b717f627ab4dc20964
Colin Pace <colinpace0001@gmail.com>
2020-09-29 03:21:04 -0500

    depth first search in Java

---
+++ depthFirstSearch.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static ArrayList <Node> visited = new ArrayList <Node>();
+  public static ArrayList <Integer> order = new ArrayList <Integer>();
+
+  public static ArrayList depthFirstSearch(Node node) {
+    if (visited.contains(node)) return null;
+    else {
+      visited.add(node);
+      order.add(node.data);
+      for (int i = 0; i < node.adj.size(); i++) {
+        if (!visited.contains(node.adj.get(i))) {
+          depthFirstSearch(node.adj.get(i));
+        }
+      }
+      return order;
+    }
+  }
+  
+
+  public static void main(String[] args) {
+    Node first = new Node(1);
+    Node second = new Node(2);
+    Node third = new Node(3);
+    Node fourth = new Node(4);
+    Node fifth = new Node(5);
+
+    first.adj.add(second);
+    second.adj.add(first);
+    second.adj.add(third);
+    second.adj.add(fourth);
+    third.adj.add(second);
+    third.adj.add(fifth);
+    fourth.adj.add(second);
+    fourth.adj.add(fifth);
+    fifth.adj.add(third);
+    fifth.adj.add(fourth);
+
+    System.out.print("Depth first search: ");
+    System.out.println(depthFirstSearch(first));
+  }
+}
+
+
+class Node {
+  int data;
+  ArrayList <Node> adj = new ArrayList <Node>();
+
+  public Node(int data) {
+    this.data = data;
+    this.adj = adj;
+  }
+}
