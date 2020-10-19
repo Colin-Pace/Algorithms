d0d518e7bcfd0941bccb29c46556d5ed828c53d9
Colin Pace <colinpace0001@gmail.com>
2020-10-19 04:34:18 -0500

    added test

--- recoverBinarySearchTree.js
+++ recoverBinarySearchTree.js
@@ -25,6 +25,7 @@ class BinarySearchTree {
     let i = 0;
     while (i < integer) {
       const element = Math.floor(Math.random() * 100);
+
       if (!this.elements.includes(element)) {
         tree.add(element);
         this.elements.push(element);
@@ -35,35 +36,41 @@ class BinarySearchTree {
 
   add(data) {
     let node = this.root;
+
     if (!node) this.root = new Node(data);
     else {
       const searchTree = function(node) {
+
         if (data < node.data) {
           if (!node.left) node.left = new Node(data);
           else return searchTree(node.left);
+
         } else if (data > node.data) {
           if (!node.right) node.right = new Node(data);
           else return searchTree(node.right);
+
         } else return null;
       }
+
       searchTree(node);
     }
   }
 
   inOrder() {
     const result = [];
+
     const traversal = function(node) {
       node.left && traversal(node.left);
       result.push(` ${node.data}`);
       node.right && traversal(node.right);
     }
+
     traversal(this.root);
     return result;
   }
 
   swap() {
     let count = this.elements.length;
-    //let count = 7;
     this.elementOne = Math.floor(Math.random() * count + 1);
     this.elementTwo = Math.floor(Math.random() * count + 1);
 
@@ -75,9 +82,11 @@ class BinarySearchTree {
       if (tree.itr < 0) return;
 
       node.left && searchTree(node.left);
+
       if (tree.itr === 1) {
         tree.elementOne ? tree.elementTwo = node : tree.elementOne = node;
       }
+
       tree.itr--;
 
       node.right && searchTree(node.right);
@@ -137,7 +146,7 @@ class BinarySearchTree {
         tree.previous.data = node.data;
         node.data = temp;
 
-        nodeFound = false;                    // how to optamize?
+        nodeFound = false;                    // how to optimize?
         return;
       }
 
@@ -174,13 +183,66 @@ class BinarySearchTree {
 
     traversal(this.root);
   }
+
+  quickSort(integers) {
+    if (integers.length < 2) return integers;
+    else {
+      const left = [];
+      const right = [];
+      const sorted = [];
+      const pivot = integers.pop();
+      const length = integers.length;
+
+      for (let i = 0; i < length; i++) {
+        if (integers[i] <= pivot) {
+          left.push(integers[i]);
+        } else right.push(integers[i]);
+      }
+
+      return sorted.concat(
+        this.quickSort(left),
+        pivot,
+        this.quickSort(right)
+      );
+    }
+  }
+
+  test() {
+    this.createElements(10);
+    const sorted = this.quickSort(tree.elements);
+    const elementsToSwap = tree.findSwap();
+    const length = this.elements.length;
+    tree.recoverTree(elementsToSwap);
+
+    this.itr = 0;
+    const inOrder = function(node) {
+      node.left && inOrder(node.left);
+
+      if (node.data != sorted[tree.itr]) return false;
+      else tree.itr++;
+
+      node.right && inOrder(node.right);
+    }
+
+    inOrder(this.root);
+
+    return true;
+  }
 }
 
 
-const tree = new BinarySearchTree;
-tree.createElements(10);
-const elementsToSwap = tree.findSwap();
+let tree;
+let test = true;
+function testRecoverBST() {
+  let i = 0;
 
-console.log(tree.inOrder());
-tree.recoverTree(elementsToSwap);
-console.log(tree.inOrder());
+  while (i < 10) {
+    tree = new BinarySearchTree;
+    if (tree.test() != true) test = false;
+    i++;
+  }
+
+  return test;
+}
+
+console.log("Tests pass: " + testRecoverBST());
