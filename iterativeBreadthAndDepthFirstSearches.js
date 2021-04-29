ad725ddccd95ad597b0c800b846d80f017fad745
Colin Pace <colinpace0001@gmail.com>
2021-04-28 21:23:14 -0500

    added edges to the time cmoplexity

--- iterativeBreadthAndDepthFirstSearches.js
+++ iterativeBreadthAndDepthFirstSearches.js
@@ -1,7 +1,7 @@
 /* A comparison of iterative breadth and depth first searches of a graph
 
 1. The data structure that contains information about whether a node has been visited, enables a more efficient search algorithm
-        a. O (n * m * p) versus O (n * m), where n is the graph, m is a vertex's adjacent vertices, and p is the order    
+        a. O ((n * m * p) + e) versus O ((n * m) + e), where n is the graph, m is a vertex's adjacent vertices, p is the order, and e is the edges   
 
 2. Graph
 
