21f98d2e9221b390360db80d24bdbea077bf94f0
Colin Pace <colinpace0001@gmail.com>
2021-05-21 18:32:50 -0500

    updates

--- bstTraversals.js
+++ bstTraversals.js
@@ -1,277 +1,214 @@
-class QueueNode {
-  constructor(data, next) {
+class Node {
+  constructor(data, adj, visited) {
     this.data = data;
-    this.next = next;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  enqueue(data) {
-    if (!this.head) {
-      this.head = new QueueNode(data);
-      this.tail = this.head;
-    } else {
-      const node = new QueueNode(data);
-      this.tail.next = node;
-      this.tail = node;
-    }
-  }
-
-  dequeue() {
-    let node;
-    if (!this.head) return null;
-    else if (this.head === this.tail) {
-      node = this.head;
-      this.head = null;
-      this.tail = null;
-      const data = node.data;
-      node = null;
-      return data;
-    } else {
-      node = this.head;
-      this.head = this.head.next;
-      const data = node.data;
-      node = null;
-      return data;
-    }
-  }
-}
-
-class GraphNode {
-  constructor(data, adj) {
-    this.data = data;
-    this.adj = adj;
-  }
-}
-
-class TreeNode {
-  constructor(data, left, right) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
+    this.adj = [];
+    this.visited = false;
   }
 }
 
 class BinarySearchTree {
   constructor() {
     this.root = null;
-    this.visited = [];
-    this.count = 0;
   }
 
   add(data) {
-    if (!data) return null;
-    else {
-      let node = this.root;
-      if (!node) this.root = new TreeNode(data);
-      else {
-        const searchTree = function(node) {
-          if (data < node.data) {
-            if (!node.left) node.left = new TreeNode(data);
-            else return searchTree(node.left);
-          } else if (data > node.data) {
-            if (!node.right) node.right = new TreeNode(data);
-            else return searchTree(node.right);
-          } else return null;
-        }
-        searchTree(node);
-      }
-    }
-  }
-
-  remove(data) {
-    if (!data || !this.root) return null;
-    else {
-      const remove = function(node, data) {
-        if (!node || !data) return null;
-
-        else if (data === node.data) {
-          if (!node.left && !node.right) return null;
-          else if (!node.right) return node.left;
-
-          else {
-            let temp = node.right;
-            let itr = temp;
-            while (temp.left) {
-              itr = temp;
-              temp = temp.left;
-            }
-
-            node.data = temp.data;
-            if (itr === temp && (!temp.left && !temp.right)) {
-              node.right = null;
-
-            } else if (!temp.left && !temp.right) itr.left = null;
-            else node.right = remove(node.right, temp.data);
-            return node;
+    if (!this.root) {
+      this.root = new Node(data);
+    } else {
+      let itr = this.root;
+      let placeFound = false;
+      while (placeFound === false) {
+        if (data < itr.data) {
+          if (!itr.left) {
+            placeFound = true;
+            itr.left = new Node(data);
+          } else {
+            itr = itr.left;
           }
-
-        } else if (data < node.data) {
-          node.left = remove(node.left, data);
-          return node;
-
         } else {
-          node.right = remove(node.right, data);
-          return node;
+          if (!itr.right) {
+            placeFound = true;
+            itr.right = new Node(data);
+          } else {
+            itr = itr.right;
+          }
         }
       }
-
-      this.root = remove(this.root, data);
-    }
-  }
-
-  inOrder() {
-    if (!this.root) return null;
-    else {
-      const result = [];
-      const traversal = function(node) {
-        node.left && traversal(node.left);
-        result.push(` ${node.data}`);
-        node.right && traversal(node.right);
-      }
-      traversal(this.root);
-      return result;
     }
   }
 
   preOrder() {
-    if (!this.root) return null;
-    else {
-      const result = [];
-      const traversal = function(node) {
-        result.push(` ${node.data}`);
-        node.left && traversal(node.left);
-        node.right && traversal(node.right);
+    const order = [];
+    const stack = [];
+    stack.push(this.root);
+    while (stack.length) {
+      const vertex = stack.pop();
+      order.push(vertex.data);
+      if (vertex.right) {
+        stack.push(vertex.right);
       }
-      traversal(this.root);
-      return result;
+      if (vertex.left) {
+        stack.push(vertex.left);
+      }
     }
+    return order;
+  }
+
+  inOrder() {
+    const order = [];
+    const stack = [];
+    let fast = this.root;
+    while (true) {
+      while (fast) {
+        stack.push(fast);
+        fast = fast.left;
+      }
+      if (!stack.length) {
+        break;
+      } else {
+        const slow = stack.pop();
+        order.push(slow.data);
+        fast = slow.right;
+      }
+    }
+    return order;
   }
 
   postOrder() {
-    if (!this.root) return null;
-    else {
-      const result = [];
-      const traversal = function(node) {
-        node.left && traversal(node.left);
-        node.right && traversal(node.right);
-        result.push(` ${node.data}`);
+    const order = [];
+    const stack = [];
+    let fast = this.root;
+    let slow = this.root;
+    while (fast) {
+      while (fast.left) {
+        stack.push(fast);
+        fast = fast.left;
       }
-      traversal(this.root);
-      return result;
+      while (!fast.right || fast.right === slow) {
+        order.push(fast.data);
+        slow = fast;
+        if (!stack.length) {
+          return order;
+        } else {
+          fast = stack.pop();
+        }
+      }
+      stack.push(fast);
+      fast = fast.right;
     }
+    return order;
   }
 
   levelOrder() {
-    if (!this.root) return null;
-    else {
-      const result = [];
-      queue.enqueue(this.root);
-      while (queue.head) {
-        const node = queue.dequeue().data;
-        result.push(" " + node.data);
-        if (node.left) queue.enqueue(node.left);
-        if (node.right) queue.enqueue(node.right);
+    const order = [];
+    const q = [];
+    q.push(this.root);
+    while (q.length) {
+      const vertex = q.shift();
+      order.push(vertex.data);
+      if (vertex.left) {
+        q.push(vertex.left);
       }
-      return result;
-    }
-  }
-
-  findMin() {
-    if (!this.root) return null;
-    else {
-      let current = this.root;
-      while (current.left) current = current.left;
-      return current;
-    }
-  }
-
-  findMax() {
-    if (!this.root) return null;
-    else {
-      let current = this.root;
-      while (current.right) current = current.right;
-      return current;
-    }
-  }
-
-  find(value) {
-    if (!this.root || !value) return null;
-    else {
-      let current = this.root;
-      while (current.data !== value) {
-        if (value < current.data) current = current.left;
-        else current = current.right;
-        if (current === null) return null;
-      }
-      return current;
-    }
-  }
-
-  minHeight(node = this.root) {
-    if (!this.root) return null;
-    else {
-      if (this.visited.includes(node)) return 0;
-      else if (node === null) return;
-      else {
-        this.visited.push(node);
-        let left = this.minHeight(node.left);
-        let right = this.minHeight(node.right);
-        if (left < right) return left + 1;
-        else return right + 1;
+      if (vertex.right) {
+        q.push(vertex.right);
       }
     }
+    return order;
   }
 
-  maxHeight(node = this.root) {
-    if (!this.root) return null;
-    else {
-      if (this.visited.includes(node)) return 0;
-      else if (node === null) return;
-      else {
-        this.visited.push(node);
-        let left = this.maxHeight(node.left);
-        let right = this.maxHeight(node.right);
-        if (left > right) return left + 1;
-        else return right + 1;
+  height() {
+    let height = 0;
+    let nodesInLevel = 1;
+    const q = [];
+    q.push(this.root);
+    while (q.length) {
+      const vertex = q.shift();
+      nodesInLevel--;
+      if (vertex.left) {
+        q.push(vertex.left);
+      }
+      if (vertex.right) {
+        q.push(vertex.right);
+      }
+      if (nodesInLevel === 0) {
+        height++;
+        nodesInLevel = q.length;
       }
     }
+    return height;
   }
 
-  balanced() {
-    if (!this.root) return null;
-    else {
-      const min = this.minHeight();
-      this.visited = [];
-      const max = this.maxHeight();
-      if (Math.abs((max - min) + 1 <= 1)) return true;
-      else return false;
+  recursivePreOrder() {
+    const order = [];
+    const traversal = function(node) {
+      order.push(node.data);
+      node.left && traversal(node.left);
+      node.right && traversal(node.right);
     }
+    traversal(this.root);
+    return order;
+  }
+  
+  recursivePreOrder() {
+    const order = [];
+    const traversal = function(node) {
+      order.push(node.data);
+      node.left && traversal(node.left);
+      node.right && traversal(node.right);
+    }
+    traversal(this.root);
+    return order;
   }
 
-  sum(node = this.root) {
-    if (!this.root) return null;
-    else {
-      this.count += node.data;
-      if (!node.left && !node.right) return node.data;
-      else {
-        node.left && this.sum(node.left);
-        node.right && this.sum(node.right);
+  recursiveInOrder() {
+    const order = [];
+    const traversal = function(node) {
+      node.left && traversal(node.left);
+      order.push(node.data);      
+      node.right && traversal(node.right);
+    }
+    traversal(this.root);
+    return order;
+  }
+  
+  recursivePostOrder() {
+    const order = [];
+    const traversal = function(node) {
+      node.left && traversal(node.left);
+      node.right && traversal(node.right);
+      order.push(node.data);
+    }
+    traversal(this.root);
+    return order;
+  }
+
+  recursiveHeight(node = this.root) {
+    if (!node) {
+      return;
+    } else if (node.visited === true) {
+      return 0;
+    } else {
+      node.visited = true;
+      const left = this.recursiveHeight(node.left);
+      const right = this.recursiveHeight(node.right);
+      if (left < right) {
+        return right + 1;
+      } else {
+        return left + 1;
       }
-      return this.count;
     }
   }
 }
 
-const queue = new Queue;
 const tree = new BinarySearchTree;
-
-const integers = [100, 150, 80, 50, 90, 130, 200, 110, 120, 115];
+const integers = [10, 5, 7, 6, 2, 1, 15, 12, 14, 20, 25];
 integers.forEach(integer => tree.add(integer));
-
-console.log(tree.root.data);
+console.log(tree.preOrder());
+console.log(tree.inOrder());
+console.log(tree.postOrder());
+console.log(tree.levelOrder());
+console.log(tree.height());
+console.log(tree.recursivePreOrder());
+console.log(tree.recursiveInOrder());
+console.log(tree.recursivePostOrder());
+console.log(tree.recursiveHeight());
