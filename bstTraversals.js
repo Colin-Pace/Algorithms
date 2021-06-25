42bfc191fad2ef94d9f5096d215d06c98b67b608
Colin Pace <colinpace0001@gmail.com>
2021-06-24 21:08:05 -0500

    added interpretation of big O to binary search tree traversals

--- bstTraversals.js
+++ bstTraversals.js
@@ -1,3 +1,45 @@
+/* Big O
+
+1. Add
+    a. Time: O(log v + e)
+    b. Space: O(1)
+
+2. Pre order
+    a. Time: O(v + e)
+    b. Space: O(1)
+
+3. In order
+    a. Time: O(v + e)
+    b. Space: O(1)
+
+4. Post order
+    a. Time: O(v + e)
+    b. Space: O(1)
+
+5. Level order
+    a. Time: O(v + e)
+    b. Space: O(1)
+
+6. Height
+    a. Time: O(v + e)
+    b. Space: O(1)
+
+7. Recursive pre order
+    a. Time: O(v + e)
+    b. Space: O(n)
+
+8. Recursive in order
+    a. Time: O(v + e)
+    b. Space: O(n)
+
+9. Recursive post order
+    a. Time: O(v + e)
+    b. Space: O(n)
+
+10. Recursive height
+    a. Time: O(v + e)
+    b. Space: O(n)       */
+
 class Node {
   constructor(data, adj, visited) {
     this.data = data;
@@ -14,9 +56,11 @@ class BinarySearchTree {
   add(data) {
     if (!this.root) {
       this.root = new Node(data);
+   
     } else {
       let itr = this.root;
       let placeFound = false;
+   
       while (placeFound === false) {
         if (data < itr.data) {
           if (!itr.left) {
@@ -25,6 +69,7 @@ class BinarySearchTree {
           } else {
             itr = itr.left;
           }
+   
         } else {
           if (!itr.right) {
             placeFound = true;
@@ -41,16 +86,20 @@ class BinarySearchTree {
     const order = [];
     const stack = [];
     stack.push(this.root);
+  
     while (stack.length) {
       const vertex = stack.pop();
       order.push(vertex.data);
+   
       if (vertex.right) {
         stack.push(vertex.right);
       }
+   
       if (vertex.left) {
         stack.push(vertex.left);
       }
     }
+   
     return order;
   }
 
@@ -58,11 +107,13 @@ class BinarySearchTree {
     const order = [];
     const stack = [];
     let fast = this.root;
+   
     while (true) {
       while (fast) {
         stack.push(fast);
         fast = fast.left;
       }
+   
       if (!stack.length) {
         break;
       } else {
@@ -71,31 +122,38 @@ class BinarySearchTree {
         fast = slow.right;
       }
     }
+   
     return order;
   }
 
   postOrder() {
     const order = [];
     const stack = [];
+    
     let fast = this.root;
     let slow = this.root;
+    
     while (fast) {
       while (fast.left) {
         stack.push(fast);
         fast = fast.left;
       }
+    
       while (!fast.right || fast.right === slow) {
         order.push(fast.data);
         slow = fast;
+    
         if (!stack.length) {
           return order;
         } else {
           fast = stack.pop();
         }
       }
+    
       stack.push(fast);
       fast = fast.right;
     }
+    
     return order;
   }
 
@@ -103,16 +161,20 @@ class BinarySearchTree {
     const order = [];
     const q = [];
     q.push(this.root);
+  
     while (q.length) {
       const vertex = q.shift();
       order.push(vertex.data);
+  
       if (vertex.left) {
         q.push(vertex.left);
       }
+  
       if (vertex.right) {
         q.push(vertex.right);
       }
     }
+  
     return order;
   }
 
@@ -121,76 +183,80 @@ class BinarySearchTree {
     let nodesInLevel = 1;
     const q = [];
     q.push(this.root);
+  
     while (q.length) {
       const vertex = q.shift();
       nodesInLevel--;
+  
       if (vertex.left) {
         q.push(vertex.left);
       }
+  
       if (vertex.right) {
         q.push(vertex.right);
       }
+  
       if (nodesInLevel === 0) {
         height++;
         nodesInLevel = q.length;
       }
     }
+  
     return height;
   }
 
   recursivePreOrder() {
     const order = [];
+ 
     const traversal = function(node) {
       order.push(node.data);
       node.left && traversal(node.left);
       node.right && traversal(node.right);
     }
     traversal(this.root);
-    return order;
-  }
-  
-  recursivePreOrder() {
-    const order = [];
-    const traversal = function(node) {
-      order.push(node.data);
-      node.left && traversal(node.left);
-      node.right && traversal(node.right);
-    }
-    traversal(this.root);
+
     return order;
   }
 
   recursiveInOrder() {
     const order = [];
+   
     const traversal = function(node) {
       node.left && traversal(node.left);
       order.push(node.data);      
       node.right && traversal(node.right);
     }
     traversal(this.root);
+   
     return order;
   }
   
   recursivePostOrder() {
     const order = [];
+  
     const traversal = function(node) {
       node.left && traversal(node.left);
       node.right && traversal(node.right);
       order.push(node.data);
     }
     traversal(this.root);
+  
     return order;
   }
 
   recursiveHeight(node = this.root) {
     if (!node) {
       return;
+  
     } else if (node.visited === true) {
       return 0;
+  
     } else {
       node.visited = true;
+  
       const left = this.recursiveHeight(node.left);
       const right = this.recursiveHeight(node.right);
+  
       if (left < right) {
         return right + 1;
       } else {
@@ -203,6 +269,7 @@ class BinarySearchTree {
 const tree = new BinarySearchTree;
 const integers = [10, 5, 7, 6, 2, 1, 15, 12, 14, 20, 25];
 integers.forEach(integer => tree.add(integer));
+
 console.log(tree.preOrder());
 console.log(tree.inOrder());
 console.log(tree.postOrder());
