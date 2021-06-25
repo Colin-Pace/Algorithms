b4bb228b4d352b9c5dda96c4b38dc5697ebbd148
Colin Pace <colinpace0001@gmail.com>
2021-06-24 20:40:09 -0500

    added interpretation of big O to stack and queue

--- stackAndQueue.js
+++ stackAndQueue.js
@@ -1,3 +1,17 @@
+/* Big O
+1. Push
+    a. Time: O(1)
+    b. Space: O(1)
+2. Pop
+    a. Time: O(1)
+    b. Space: O(1)
+3. Enqueue
+    a. Time: O(1)
+    b. Space: O(1)
+4. Dequeue
+    a. Time: O(1)
+    b. Space: O(1) */
+
 class Node {
   constructor(data, next) {
     this.data = data;
@@ -15,6 +29,7 @@ class Stack {
     if (!this.head) {
       this.head = new Node(data);
       this.tail = this.head;
+    
     } else {
       const node = new Node(data);
       node.next = this.tail;
@@ -24,14 +39,17 @@ class Stack {
 
   pop() {
     let node;
-    if (!this.head) return null;
-    else if (this.head === this.tail) {
+    if (!this.head) {
+      return null;
+    
+    } else if (this.head === this.tail) {
       node = this.head;
       this.head = null;
       this.tail = null;
       const data = node.data;
       node = null;
       return data;
+    
     } else {
       node = this.tail;
       this.tail = this.tail.next;
@@ -52,6 +70,7 @@ class Queue {
     if (!this.head) {
       this.head = new Node(data);
       this.tail = this.head;
+    
     } else {
       const node = new Node(data);
       this.tail.next = node;
@@ -61,14 +80,17 @@ class Queue {
 
   dequeue() {
     let node;
-    if (!this.head) return null;
-    else if (this.head === this.tail) {
+    if (!this.head) {
+      return null;
+    
+    } else if (this.head === this.tail) {
       node = this.head;
       this.head = null;
       this.tail = null;
       const data = node.data;
       node = null;
       return data;
+    
     } else {
       node = this.head;
       this.head = this.head.next;
