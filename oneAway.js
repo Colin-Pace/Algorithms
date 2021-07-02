8d6407cf67e3f4f4f52826556589ad5da13c4647
Colin Pace <colinpace0001@gmail.com>
2021-07-02 12:30:08 -0500

    updated answer

--- oneAway.js
+++ oneAway.js
@@ -1,44 +1,74 @@
-// Find whether two strings are one edit away from each other
+/* Find whether two strings are one edit away from each other; assume ASCII characters
 
-function oneAway(str, str_) {
-  let strObj = {};
-  let str_Obj = {};
+Big O
+1. Time: O(c + p)
+2. Space: O(1)  */
+
+
+function oneAway(strOne, strTwo) {
+  let charactersOne = {};
+  let charactersTwo = {};
   let differentLetters = 0;
-  if (Math.abs(str.length - str_.length) > 1) return false;
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] in strObj) strObj[str[i]] += 1;
-    else strObj[str[i]] = 1;
+  
+  if (Math.abs(strOne.length - strTwo.length) > 1) {
+    return false;
   }
-  for (let i = 0; i < str_.length; i++) {
-    if (str_[i] in str_Obj) str_Obj[str_[i]] += 1;
-    else str_Obj[str_[i]] = 1;
+  
+  for (let i = 0; i < strOne.length; i++) {
+    if (strOne[i] in charactersOne) {
+      charactersOne[strOne[i]] += 1;
+    } else {
+      charactersOne[strOne[i]] = 1;
+    }
   }
+  
+  for (let i = 0; i < strTwo.length; i++) {
+    if (strTwo[i] in charactersTwo) {
+      charactersTwo[strTwo[i]] += 1;
+    } else {
+      charactersTwo[strTwo[i]] = 1;
+    }
+  }
+  
   let shorter;
   let longer;
-  if (str.length === str_.length) {
-    shorter = strObj;
-    longer = str_Obj;
-  } else if (str.legnth < str_.length) {
-    shorter = strObj;
-    longer = str_Obj;
+  
+  if (strOne.length === strTwo.length) {
+    shorter = charactersOne;
+    longer = charactersTwo;
+  
+  } else if (strOne.legnth < strTwo.length) {
+    shorter = charactersOne;
+    longer = charactersTwo;
+  
   } else {
-    shorter = str_Obj;
-    longer = strObj;
+    shorter = charactersTwo;
+    longer = charactersOne;
   }
+  
   for (let i in shorter) {
-    if (Math.abs(shorter[i] - longer[i] > 1)) return false;
-    if (shorter[i] !== longer[i]) differentLetters++;
-    if (differentLetters > 1) return false;
+    if (Math.abs(shorter[i] - longer[i] > 1)) {
+      return false;
+   
+    } else if (shorter[i] !== longer[i]) {
+      differentLetters++;
+   
+    } else if (differentLetters > 1) {
+      return false;
+    }
   }
+  
   return true;
 }
+
+
 const letters = "pale";
 const additionAway = "ple";
 const replacementAway = "bale";
 const deletionAway = "pales";
 const notOneAway = "bake";
 
-console.log(oneAway(letters, additionAway));
-console.log(oneAway(letters, replacementAway));
-console.log(oneAway(letters, deletionAway));
-console.log(oneAway(letters, notOneAway));
+console.log(oneAway(letters, additionAway)); // true
+console.log(oneAway(letters, replacementAway)); // true
+console.log(oneAway(letters, deletionAway)); // true
+console.log(oneAway(letters, notOneAway)); // false 
