576f0c46feda156aa5f9b4ff58fd1d3da4812294
Colin Pace <colinpace0001@gmail.com>
2021-07-02 12:46:38 -0500

    updated answer

--- stringCompression.js
+++ stringCompression.js
@@ -1,14 +1,23 @@
-// Compress a string such that the number of letters of consecutive letters follows the letter; if the compressed string is longer than the original, return the original
+/* Compress a string such that the number of letters of consecutive letters follows the letter; if the compressed string is longer than the original, return the original
+
+Big O
+1. Time: O(b), if string concatenation in JavaScript is O(b)
+2. Space: O(b)      */
+
 
 function stringCompression(str) {
   let result = "";
   let count = 1;
+  
   for (let i = 0; i < str.length - 1; i++) {
-    if (result.length > str.length) return str;
-    if (str[i] === str[i+1]) {
+    if (result.length > str.length) {
+      return str;
+    
+    } else if (str[i] === str[i + 1]) {
       if (i + 2 === str.length) {
         count++;
         result += `${str[i]}${count}`;
+    
       } else {
         count++;
       }
@@ -16,15 +25,18 @@ function stringCompression(str) {
     } else {
       if (i + 2 === str.length) {
         result += `${str[i]}${count}`;
-        result += `${str[i+1]}1`;
+        result += `${str[i + 1]}1`;
+    
       } else {
         result += `${str[i]}${count}`;
         count = 1;
       }
     }
   }
+ 
   return result;
 }
+
 
 const letters = "aabcccccaaa";
 const lettersTwo = "abbbbbccdda";
