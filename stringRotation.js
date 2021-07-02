44663d796cf50dcaa79505567e2a547960599573
Colin Pace <colinpace0001@gmail.com>
2021-07-02 13:13:49 -0500

    removed file

--- stringRotation.js
+++ stringRotation.js
@@ -1,10 +1,15 @@
-// Check to see if one of two strings is a substring of the other, using only one call to check for the substring
+/* Check to see if one of two strings is a substring of the other, using only one call to check for the substring */
+
 
 function stringRotation(str, str_) {
   const doubled = str + str;
-  if (doubled.includes(str_)) return true;
-  else return false;
+  if (doubled.includes(str_)) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 const letters = "erbottlewat";
 const rotation = "waterbottle";
